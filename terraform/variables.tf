85c050846e0e9fa4648535d7d7b27821c7bf2421
ppanter <panterpatryk@gmail.com>
2022-03-20 09:50:36 +0100

    basic variables creation

---
+++ terraform/variables.tf
@@ -0,0 +1,18 @@
+# variable infra_region {
+#     description = "Region where infrastructure is created"
+#     type = string
+# }
+
+variable my_ip {
+    description = "My IP used to limit access to network"
+    type = string
+}
+
+variable vpc {
+    description = "Network configuration"
+    type = object({
+        cidr = string
+        subnets = map(map(string))
+    })
+}
+
